4a427ff9715201b409045b2d9ee72a3343da73e4
Llama <150065016+CharlesLopezE@users.noreply.github.com>
2026-06-04 18:16:09 -0700

    Add monster data and display functionality for Monster Hunter World
    - Created mainpage-world.js to manage the display of base and expansion monsters.
    - Implemented functions to show large and small base monsters, as well as expansion monsters.
    - Added search and sorting capabilities for both base and expansion monsters.
    - Introduced a toggle sidebar feature for better navigation.
    - Defined monster data in monsters-world.js, including base and expansion monsters.

---
+++ src/JS/mainpage-world.js
@@ -0,0 +1,250 @@
+let currentBaseMonsters = baseWorldMonsters;
+
+function showBaseLargeMonsters() {
+
+    currentBaseMonsters = baseLargeMonsters;
+
+    displayBaseMonsters(currentBaseMonsters);
+
+    document.querySelector("h2").textContent =
+        "Monster Hunter World";
+
+}
+
+function showBaseSmallMonsters() {
+
+    currentBaseMonsters = baseSmallMonsters;
+
+    displayBaseMonsters(currentBaseMonsters);
+
+    document.querySelector("h2").textContent =
+        "Monster Hunter World";
+
+}
+
+let currentExpansionMonsters = expansionIceborneMonsters;
+
+function showExpansionLargeMonsters() {
+
+    currentExpansionMonsters = expansionLargeMonsters;
+
+    displayExpansionMonsters(currentExpansionMonsters);
+
+    document.querySelector("h3").textContent =
+        "Monster Hunter World: Iceborne";
+
+}
+
+function showExpansionSmallMonsters() {
+
+    currentExpansionMonsters = expansionSmallMonsters;
+
+    displayExpansionMonsters(currentExpansionMonsters);
+
+    document.querySelector("h3").textContent =
+        "Monster Hunter World: Iceborne";
+
+}
+
+/*Toggle sidebar*/
+function toggleSidebar() {
+    const nav = document.querySelector("nav");
+
+    const button = document.getElementById("sidebarToggle");
+
+    nav.classList.toggle("collapsed");
+
+    if (nav.classList.contains("collapsed")) {
+        button.textContent = "❯";
+        button.style.left = "0px";
+    }
+    else {
+        button.textContent = "❮";
+        button.style.left = "260px";
+    }
+}
+
+const baseMonsterGrid =
+    document.getElementById("baseMonsterGrid");
+/* =========================
+   DISPLAY BASE MONSTERS
+========================= */
+function displayBaseMonsters(monstersToDisplay) {
+    baseMonsterGrid.innerHTML = "";
+    monstersToDisplay.forEach(monster => {
+
+        baseMonsterGrid.innerHTML += `
+            <div class="monster-card">
+                <div
+                    class="monster-image"
+                    data-name="${monster.name}"
+                    data-species="${monster.species}"
+                    data-rank="${monster.rank}">
+                    <img
+                        src="${monster.image}"
+                        alt="${monster.name}">
+                </div>
+                <div class="monster-name">
+                    <a href="infopage-rise.html">${monster.name}</a>
+                    <div class="monster-species">
+                        <span class="info-label"> Species:</span>
+                        
+                        <span class="info-value">${monster.species}</span>
+                    </div>
+                    <div class="monster-rank">
+                        <span class="info-label">Rank:</span>
+                        
+                        <span class="info-value-rank">${monster.rank}</span>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+}
+displayBaseMonsters(baseWorldMonsters);
+
+const expansionMonsterGrid =
+    document.getElementById("expansionMonsterGrid");
+/* =========================
+   DISPLAY EXPANSION MONSTERS
+========================= */
+function displayExpansionMonsters(monstersToDisplay) {
+    expansionMonsterGrid.innerHTML = "";
+    monstersToDisplay.forEach(monster => {
+
+        expansionMonsterGrid.innerHTML += `
+            <div class="monster-card">
+                <div
+                    class="monster-image"
+                    data-name="${monster.name}"
+                    data-species="${monster.species}"
+                    data-rank="${monster.rank}">
+                    <img
+                        src="${monster.image}"
+                        alt="${monster.name}">
+                </div>
+                <div class="monster-name">
+                    <a href="infopage-rise.html">${monster.name}</a>
+                    <div class="monster-species">
+                        <span class="info-label"> Species:</span>
+                        
+                        <span class="info-value">${monster.species}</span>
+                    </div>
+                    <div class="monster-rank">
+                        <span class="info-label">Rank:</span>
+                        
+                        <span class="info-value-rank">${monster.rank}</span>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+}
+displayExpansionMonsters(expansionIceborneMonsters);
+/* =========================
+   SEARCH
+========================= */
+function searchBaseMonster() {
+    const input =
+        document.getElementById("searchInput")
+        .value
+        .toLowerCase();
+
+    const filteredMonsters =
+        currentBaseMonsters.filter(monster =>
+            monster.name
+                .toLowerCase()
+                .includes(input)
+        );
+    displayBaseMonsters(filteredMonsters);
+}
+function searchExpansionMonster() {
+    const input =
+        document.getElementById("searchInput")
+        .value
+        .toLowerCase();
+
+    const filteredMonsters =
+        currentExpansionMonsters.filter(monster =>
+            monster.name
+                .toLowerCase()
+                .includes(input)
+        );
+    displayExpansionMonsters(filteredMonsters);
+}
+/* =========================
+   DROPDOWN
+========================= */
+function toggleDropdown() {
+    document
+        .getElementById("sortDropdown")
+        .classList
+        .toggle("open");
+}
+
+document.addEventListener("click", function(e) {
+    if (!e.target.closest(".dropdown")) {
+        document
+            .getElementById("sortDropdown")
+            .classList
+            .remove("open");
+    }
+});
+
+/* =====================================
+   SORTING BASE + EXPANSION MONSTERS
+======================================*/
+function sortBaseMonsters(method) {
+
+    let sorted = [...currentBaseMonsters];
+
+    if (method === "name") {
+
+        sorted.sort((a, b) =>
+            a.name.localeCompare(b.name)
+        );
+
+    }
+    else if (method === "name-desc") {
+
+        sorted.sort((a, b) =>
+            b.name.localeCompare(a.name)
+        );
+
+    }
+
+    displayBaseMonsters(sorted);
+}
+function sortExpansionMonsters(method) {
+
+    let sorted = [...currentExpansionMonsters];
+
+    if (method === "name") {
+
+        sorted.sort((a, b) =>
+            a.name.localeCompare(b.name)
+        );
+
+    }
+    else if (method === "name-desc") {
+
+        sorted.sort((a, b) =>
+            b.name.localeCompare(a.name)
+        );
+
+    }
+
+    displayExpansionMonsters(sorted);
+}
+
+function sortBy(method) {
+
+    sortBaseMonsters(method);
+
+    sortExpansionMonsters(method);
+
+    document
+        .getElementById("sortDropdown")
+        .classList
+        .remove("open");
+}
---
+++ src/JS/monsters-world.js
@@ -0,0 +1,29 @@
+const baseWorldMonsters = [
+    {name:"Great Jagras", species:"Fanged Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Great_Jagras_Icon.png"},
+    {name:"Kulu-Ya-Ku", species:"Bird Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Kulu-Ya-Ku_Icon.png"},
+    {name:"Pukei-Pukei", species:"Bird Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Pukei-Pukei_Icon.png"},
+    {name:"Barroth", species:"Brute Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Barroth_Icon.png"},
+    {name:"Jyuratodus", species:"Fish Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Jyuratodus_Icon.png"},
+    {name:"Tobi-Kadachi", species:"Fanged Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Tobi-Kadachi_Icon.png"},
+    {name:"Anjanath", species:"Brute Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Anjanath_Icon.png"},
+    {name:"Tzitzi-Ya-Ku", species:"Bird Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Tzitzi-Ya-Ku_Icon.png"},
+    {name:"Paolumu", species:"Bird Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Paolumu_Icon.png"},
+    {name:"Great Girros", species:"Fanged Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Great_Girros_Icon.png"},
+    {name:"Radobaan", species:"Brute Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Radobaan_Icon.png"},
+    {name:"Legiana", species:"Flying Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Legiana_Icon.png"},
+    {name:"Odogaron", species:"Brute Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Odogaron_Icon.png"},
+    {name:"Rathalos", species:"Flying Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Rathalos_Icon.png"},
+    {name:"Diablos", species:"Flying Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Diablos_Icon.png"},
+];
+
+const baseLargeMonsters = [
+    {name:"Great Jagras", species:"Fanged Wyverns", rank:"Low-Rank",image:"../../MH-Images/MonsterHunter-Images/MonsterHunterWorld/LargeMonsters-Icons/MHWI-Great_Jagras_Icon.png"},
+];
+
+const baseSmallMonsters = [
+    {}
+];
+
+const expansionIceborneMonsters = [{}];
+const expansionLargeMonsters = [{}];
+const expansionSmallMonsters = [{}];
