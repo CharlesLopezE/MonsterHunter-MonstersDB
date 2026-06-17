6b59e8f7071ad8358bc3517fce39e7df5f979875
Llama <150065016+CharlesLopezE@users.noreply.github.com>
2026-06-17 15:30:05 -0700

    Refactor main page headers and add new icons
    - Removed the logo image from the header in both mainpage-rise.html and mainpage-world.html, simplifying the header to just display "Monsters".
    - Added new icon images: MHWilds-Book_Icon_Brown.png and MHWilds-Book_Icon_Light_Green.png to the Monster Hunter Wilds icons directory.

---
+++ src/config.js
@@ -0,0 +1,4 @@
+NEXT_PUBLIC_SUPABASE_URL= "https://raclkiumgbtqlvwmpvqt.supabase.co"
+NEXT_PUBLIC_SUPABASE_PUBLICHABLE_KEY="sb_publishable_2Tq44eDe2qecfUdCFUdKAQ_WE03ukEf"
+
+const supabaseClient =supabase.creatClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_PUBLICHABLE_KEY);
